ed4afa458e1b268981ceec079c6e78e17f20038e
anshuraj <imanshuraj@gmail.com>
2017-02-20 23:39:11 +0530

    homepage

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -15,21 +15,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </head>
 <body>
 	<nav class="navbar navbar-default">
-  <div class="container">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">OCL</a>
+    <div class="container">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">OCL</a>
+      </div>
+      <div class="navbar-right">
+        <ul class="nav navbar-nav">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Catalog</a></li>
+          <li><a href="<?php echo site_url("account"); ?>">Log In</a></li>
+          <li id="register"><a href="<?php echo site_url("account"); ?>" style="color: white; ">Register</a></li>
+        </ul>
+      </div>
     </div>
-    <div class="navbar-right">
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Catalog</a></li>
-      <li><a href="#">Log In</a></li>
-      <li><a href="#">About</a></li>
-    </ul>
-    </div>
-  </div>
+  </nav>
 
-</nav>
 	<div class='container'>
 	  <div class="">
 		  <h2>Welcome to Online Course Library</h2>
@@ -37,5 +38,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       <button class="btn btn-primary">Explore</button>
     </div>
 	</div>
+
+
+
+
+<script type="text/javascript" src="<?php echo site_url("public/bootstrap/js/jquery.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo site_url("public/bootstrap/js/bootstrap.min.js"); ?>"></script>
+
 </body>
 </html>
