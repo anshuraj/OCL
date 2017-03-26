56dd4be3942ce1c327cf397e53025cf849ed3e99
anshuraj <imanshuraj@gmail.com>
2017-03-26 11:18:46 +0530

    redirect to cretate

--- application/views/teacher/dashboard.php
+++ application/views/teacher/dashboard.php
@@ -3,7 +3,7 @@
 	<div class="container">
 		<h3>Teacher Dashboard</h3>
 
-		<button class="btn btn-primary">Create course</button>
+		<button class="btn btn-primary" onclick="location.href='<?php echo site_url('teacher/create'); ?>'">Create course</button>
 
 		<?php for ($i=0; $i < 10; $i++) { 
 			echo "<ul>
