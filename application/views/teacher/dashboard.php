143cf1a2696858ede8f21fdfdfdda1021ae31677
anshuraj <imanshuraj@gmail.com>
2017-04-10 02:32:30 +0530

    display in tabular form

--- application/views/teacher/dashboard.php
+++ application/views/teacher/dashboard.php
@@ -5,12 +5,22 @@
 
 		<button class="btn btn-primary" onclick="location.href='<?php echo site_url('teacher/create'); ?>'">Create course</button>
 
-		<?php for ($i=0; $i < sizeof($courses); $i++) { 
-			echo "<ul>
-					<li><a href='#'>".$courses[$i]['name']."</a></li>
-				</ul>" ;
-		} ?>
-
+		<div class="row">
+			<table class="table table-hover">
+				<?php for ($i=0; $i < sizeof($courses); $i++) { 
+					echo '
+					    <tbody>
+					      <tr>
+					        <td><a href="'.site_url('classroom/'.$courses[$i]['id'].'/0').'">'.$courses[$i]['name'].'</a></td>
+					        <td>Grade</td>
+					        <td><a href="'.site_url('teacher/update/'.$courses[$i]['id']).'">Update</a></td>
+					        <td>Remove</td>
+					      </tr>
+					    </tbody>
+					  ';
+				} ?>
+				</table>
+		</div>
 	</div>
 
 
